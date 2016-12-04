d28e9d2059dcc7a7fd59efa7aa12099bbff3286a
Sparsh <sidanasparsh@gmail.com>
2016-12-04 00:30:59 -0800

    Added logic to commit to deliver e ail to the rigth user

--- Have A Seat /main.py
+++ Have A Seat /main.py
@@ -486,8 +486,9 @@ def seatsBooked():
         db.Tables.update({"Restid": restID, "TableNo": int(table["sid"])},{'$set': {"isAvailable":int(table["status"])}}, upsert=False)
         print "updated", table["sid"]
         counter=counter+1
-    if counter>0:
-        emailCustomer(counter,bookedRest['restName'], bookedRest['City'])
+        currentUser=db.Customers.find_one({'Email':session['Email']})
+    if counter>0 and currentUser:
+        emailCustomer(counter,bookedRest['restName'], bookedRest['City'], currentUser['Email'])
 
 
     dict = {}
@@ -652,15 +653,15 @@ def logout():
         return 'Logged out'
     return "noone is logged in"
 
-def emailCustomer(counter, name, city):
+def emailCustomer(counter, name, city, email):
     sender ="haveaseat.team5@gmail.com"
-    receiver = "sidanasparsh@gmail.com"
+    receiver = email
     message = MIMEMultipart()
     message['From'] = sender
     message['To'] = receiver
     message['Subject'] = "Seats Booked, Happy Dining!"
     print("Please reach the restaurant within the next 15 minutes!")
-    body = "You have booked "+str(counter)+" seats(s) at " + " "+ name+", "+ city + ". Please reach the restaurant within the next 15 minutes"
+    body = "You have booked "+str(counter)+" seats(s) at " + " "+ name + ". Please reach the restaurant within the next 15 minutes"
     message.attach(MIMEText(body, 'plain'))
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
